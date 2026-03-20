1447414fc8defc6e93e2de15cf56f680adee7501
HarshaAdithya317 <harshaadithyagorre@gmail.com>
2026-03-20 23:39:02 +0530

    Initial commit for final version

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+import analyzeRoute from "../backend/routes/analyze.js";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/analyze", analyzeRoute);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
